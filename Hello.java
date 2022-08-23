90bae081a792a617547ce0493e88cd96c4001f6c
burntricee <101063146+burntricee@users.noreply.github.com>
2022-08-23 18:23:12 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*
+* Java Program Name
+* Samuel Sarzaba
+* 8/23/2022
+* 255 001
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Your Name>");
+		System.out.println("<Java Program Name>");
+		System.out.println("<Program/Project Description>");
+		System.out.println("<Date Created>");
+		System.out.println("<Course Number and Selection Number>");
+		System.out.println();
+	}
+}
